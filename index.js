f9624fd89ccccc33a1c3cd4ceeddfeb472014990
Nayeem Uzzaman <n.nayeem.rm@gmail.com>
2023-05-16 02:59:23 +0600

    updated

--- index.js
+++ index.js
@@ -159,10 +159,98 @@ app.get("/api/multivariable",(req,res)=>{
         }
       ]})
 })
-app.get("/api/automatamcq",(req,res)=>{
-    res.json({"data":[  {    "question": "Which of the following represents a scalar quantity?",    "A": "Vector",    "B": "Matrix",    "C": "Scalar",    "D": "Tensor",    "correct": "C"  },  {    "question": "What is the determinant of the 2x2 matrix [[3, 4], [2, -1]]?",    "A": "5",    "B": "7",    "C": "-7",    "D": "-5",    "correct": "D"  },  {    "question": "Which of the following is a valid dot product of two vectors?",    "A": "Vector × Scalar",    "B": "Matrix × Vector",    "C": "Vector × Vector",    "D": "Scalar × Matrix",    "correct": "C"  },  {    "question": "What is the rank of a 3x3 identity matrix?",    "A": "1",    "B": "2",    "C": "3",    "D": "0",    "correct": "C"  },  {    "question": "Which of the following is a valid eigenvalue of a symmetric matrix?",    "A": "Complex number",    "B": "Negative number",    "C": "Positive number",    "D": "All eigenvalues can be any real number",    "correct": "D"  },  {    "question": "What is the null space of a matrix?",    "A": "The set of all row vectors in the matrix",    "B": "The set of all column vectors in the matrix",    "C": "The set of all vectors orthogonal to the rows of the matrix",    "D": "The set of all vectors that satisfy Ax = 0",    "correct": "D"  },  {    "question": "Which of the following matrices is orthogonal?",    "A": "[[1, 0], [0, 1]]",    "B": "[[1, 1], [-1, 1]]",    "C": "[[2, 0], [0, 2]]",    "D": "[[0, 1], [-1, 0]]",    "correct": "A"  }]
+app.get("/api/mcq/automata",(req,res)=>{
+  res.json({"data":[{
+    "question": "Which of the following is not a type of automaton?",
+    "A": "Finite Automaton (FA)",
+    "B": "Pushdown Automaton (PDA)",
+    "C": "Turing Machine (TM)",
+    "D": "Deterministic Finite Automaton (DFA)",
+    "correct": "D"
+  },
+  {
+    "question": "What is the language accepted by a Finite Automaton (FA) called?",
+    "A": "Deterministic Language",
+    "B": "Regular Language",
+    "C": "Context-Free Language",
+    "D": "Unrestricted Language",
+    "correct": "B"
+  },
+  {
+    "question": "Which of the following automata can recognize context-free languages?",
+    "A": "Deterministic Pushdown Automaton (DPDA)",
+    "B": "Nondeterministic Finite Automaton (NFA)",
+    "C": "Turing Machine (TM)",
+    "D": "Deterministic Finite Automaton (DFA)",
+    "correct": "A"
+  },
+  {
+    "question": "Which of the following is true about non-deterministic automata?",
+    "A": "They have a unique transition for each input symbol and state",
+    "B": "They can be simulated by deterministic automata",
+    "C": "They accept a proper subset of the languages accepted by deterministic automata",
+    "D": "They cannot recognize regular languages",
+    "correct": "C"
+  },
+  {
+    "question": "What is the equivalent computational power of a Turing Machine (TM)?",
+    "A": "DFA",
+    "B": "PDA",
+    "C": "NFA",
+    "D": "All of the above",
+    "correct": "D"
+  },
+  {
+    "question": "Which of the following is true about regular languages?",
+    "A": "They can be recognized by Turing Machines",
+    "B": "They are closed under union, concatenation, and Kleene star operations",
+    "C": "They can only accept finite languages",
+    "D": "They require a stack for recognition",
+    "correct": "B"
+  },
+  {
+    "question": "Which of the following is a characteristic of a context-free language?",
+    "A": "They can be recognized by deterministic pushdown automata",
+    "B": "They require a two-dimensional tape for computation",
+    "C": "They have a linear amount of memory",
+    "D": "They are closed under complementation",
+    "correct": "A"
+  },
+  {
+    "question": "What is the result of concatenating two regular languages?",
+    "A": "A context-free language",
+    "B": "An unrestricted language",
+    "C": "A non-regular language",
+    "D": "Another regular language",
+    "correct": "D"
+  },
+  {
+    "question": "Which of the following is not a property of a regular language?",
+    "A": "Closed under complementation",
+    "B": "Closed under intersection",
+    "C": "Closed under union",
+    "D": "Closed under reversal",
+    "correct": "A"
+  }]
 })
+})
+app.get("/api/mcq/datastructure",(req,res)=>{
+  res.json({"data":[]
+})
+})
+app.get("/api/mcq/microcontroller",(req,res)=>{
+  res.json({"data":[]
+})
+})
+app.get("/api/mcq/mvc",(req,res)=>{
+  res.json({"data":[]
+})
+})
+app.get("/api/mcq/linearAlgebra",(req,res)=>{
+    res.json({"data":[  {    "question": "Which of the following represents a scalar quantity?",    "A": "Vector",    "B": "Matrix",    "C": "Scalar",    "D": "Tensor",    "correct": "C"  },  {    "question": "What is the determinant of the 2x2 matrix [[3, 4], [2, -1]]?",    "A": "5",    "B": "7",    "C": "-7",    "D": "-5",    "correct": "D"  },  {    "question": "Which of the following is a valid dot product of two vectors?",    "A": "Vector × Scalar",    "B": "Matrix × Vector",    "C": "Vector × Vector",    "D": "Scalar × Matrix",    "correct": "C"  },  {    "question": "What is the rank of a 3x3 identity matrix?",    "A": "1",    "B": "2",    "C": "3",    "D": "0",    "correct": "C"  },  {    "question": "Which of the following is a valid eigenvalue of a symmetric matrix?",    "A": "Complex number",    "B": "Negative number",    "C": "Positive number",    "D": "All eigenvalues can be any real number",    "correct": "D"  },  {    "question": "What is the null space of a matrix?",    "A": "The set of all row vectors in the matrix",    "B": "The set of all column vectors in the matrix",    "C": "The set of all vectors orthogonal to the rows of the matrix",    "D": "The set of all vectors that satisfy Ax = 0",    "correct": "D"  },  {    "question": "Which of the following matrices is orthogonal?",    "A": "[[1, 0], [0, 1]]",    "B": "[[1, 1], [-1, 1]]",    "C": "[[2, 0], [0, 2]]",    "D": "[[0, 1], [-1, 0]]",    "correct": "A"  }]
+  })
 })
 
 
 app.listen(5000,()=>{console.log("server started on 5000")})
+  
